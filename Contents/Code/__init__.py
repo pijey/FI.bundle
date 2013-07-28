ccbf0a604da84e61b2d77dab97192d8e3623fa05
mmccurdy07 <mmccurdy@gmail.com>
2013-07-28 14:24:12 -0700

    Use proper official name: Resonance104.4fm.

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -1,6 +1,6 @@
 ART = 'art-default.jpg'
 ICON = 'icon-default.jpg'
-NAME = 'Resonance FM 104.4'
+NAME = 'Resonance104.4fm'
 STREAM_URL = 'http://radio.canstream.co.uk:8004/live.mp3'
 
 ####################################################################################################
